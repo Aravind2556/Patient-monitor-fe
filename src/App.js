091923b3b1cdec8a4c52c5edb6e71074b9b08e15
keerthivatshiniramesh <keerthivarshini.ramesh@gmail.com>
2026-01-06 15:53:04 +0530

    update the Authentication

--- src/App.js
+++ src/App.js
@@ -5,28 +5,28 @@ import Register from './components/pages/Register';
 import { useContext } from 'react';
 import { DContext } from './context/Datacontext';
 import LoadingPage from './components/pages/Loading';
-import Home from './components/pages/Home';
+import Home from './components/pages/DoctorHomePage';
 import Header from './components/blocks/Header';
 
 
 function App() {
 
-  const {isAuth, currentUser} = useContext(DContext)
+  const { isAuth, currentUser } = useContext(DContext)
 
-  if(isAuth===null || !currentUser){
-    return <LoadingPage/>
+  if (isAuth === null || !currentUser) {
+    return <LoadingPage />
   }
 
   return (
     <div className="container-fluid p-0">
-      <Header/>
+      {isAuth && <Header />}
       <Routes>
-        <Route path="/" element={isAuth?<Home/>:<Login/>} />
-        <Route path="/login" element={isAuth?<Home/>:<Login/>} />
-        <Route path='/register' element={isAuth?<Home/>:<Register/>} />
-        <Route path='/test' element={<LoadingPage/>} />
+        <Route path="/" element={isAuth ? <Home /> : <Login />} />
+        <Route path="/login" element={isAuth ? <Home /> : <Login />} />
+        <Route path='/register' element={isAuth ? <Home /> : <Register />} />
+        <Route path='/test' element={<LoadingPage />} />
       </Routes>
-     
+
     </div>
   );
 }
