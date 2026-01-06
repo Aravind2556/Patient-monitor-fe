d9d4b0bac7ab673d47c4f2a3d09f663d285e9b66
keerthivatshiniramesh <keerthivarshini.ramesh@gmail.com>
2026-01-06 14:51:14 +0530

    add context file and components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
-import React from 'react'
+import { Route, Routes } from 'react-router-dom';
+import './css/App.css';
+import Login from './components/pages/Login';
+import Register from './components/pages/Register';
+import { useContext } from 'react';
+import { DContext } from './context/Datacontext';
+import LoadingPage from './components/pages/Loading';
+import Home from './components/pages/Home';
+import Header from './components/blocks/Header';
 
-export const App = () => {
+
+function App() {
+
+  const {isAuth, currentUser} = useContext(DContext)
+
+  if(isAuth===null || !currentUser){
+    return <LoadingPage/>
+  }
+
   return (
-    <div>App</div>
-  )
+    <div className="container-fluid p-0">
+      <Header/>
+      <Routes>
+        <Route path="/" element={isAuth?<Home/>:<Login/>} />
+        <Route path="/login" element={isAuth?<Home/>:<Login/>} />
+        <Route path='/register' element={isAuth?<Home/>:<Register/>} />
+        <Route path='/test' element={<LoadingPage/>} />
+      </Routes>
+     
+    </div>
+  );
 }
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { App } from './App';
-
+import './css/index.css';
+import './css/globals.css';
+import App from './App';
+import DataContext from './context/Datacontext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
-    <App />
+        <DataContext>
+            <App />
+        </DataContext>
     </BrowserRouter>
 );
-
 
