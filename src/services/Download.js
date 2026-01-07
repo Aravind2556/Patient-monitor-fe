98b70b3ae995af6a650925d4eae3a90ff36f4dea
keerthivatshiniramesh <keerthivarshini.ramesh@gmail.com>
2026-01-07 15:50:01 +0530

    update vibration excel

--- src/services/Download.js
+++ src/services/Download.js
@@ -1,27 +1,30 @@
-export const vibrationDownload = async ({ BeURL, id }) => {
+export const vibrationDownload = async ({ BeURL, id, isDownload }) => {
     try {
-        const res = await fetch(`${BeURL}/fetchVibration/${id}`, {
-            method: 'GET',
-            credentials: 'include'
-        });
+        if (isDownload) {
+            const res = await fetch(`${BeURL}/fetchVibration/${id}?isDownload=${isDownload}`, {
+                method: 'GET',
+                credentials: 'include'
+            });
 
-        if (!res.ok) {
-            throw new Error('Failed to download Excel');
+            if (!res.ok) {
+                throw new Error('Failed to download Excel');
+            }
+
+            const blob = await res.blob();
+
+            // Create download link
+            const url = window.URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.href = url;
+            a.download = 'Vibration_History.xlsx';
+            document.body.appendChild(a);
+            a.click();
+
+            // Cleanup
+            a.remove();
+            window.URL.revokeObjectURL(url);
         }
 
-        const blob = await res.blob();
-
-        // Create download link
-        const url = window.URL.createObjectURL(blob);
-        const a = document.createElement('a');
-        a.href = url;
-        a.download = 'Vibration_History.xlsx';
-        document.body.appendChild(a);
-        a.click();
-
-        // Cleanup
-        a.remove();
-        window.URL.revokeObjectURL(url);
 
     } catch (err) {
         console.error("Error in downloading vibration excel:", err);
