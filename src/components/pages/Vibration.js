e4ed87f0fef2779cdbdfe695d5150d11cf26f869
keerthivatshiniramesh <keerthivarshini.ramesh@gmail.com>
2026-01-07 11:33:57 +0530

    add vibration history

--- src/components/pages/Vibration.js
+++ src/components/pages/Vibration.js
@@ -1,21 +1,64 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { DContext } from '../../context/Datacontext'
 
 export const Vibration = () => {
-            const { BeURL }=useContext(DContext)
-            useEffect(()=>{
-                if(BeURL){
-                    fetch(`${BeURL}/vibration`,{
-                        method : 'GET',
-                        credentials : 'include'
-                    })
-                    .then(res=>res.json())
-                    .then(data=>{
+    const { BeURL, currentUser } = useContext(DContext)
+    console.log("currentUser90", currentUser)
+
+    const [vibrationHistory, setVibrationHistory] = useState([])
+
+    useEffect(() => {
+        if (BeURL) {
+            fetch(`${BeURL}/fetchVibration/${currentUser.id}`, {
+                method: 'GET',
+                credentials: 'include'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.success) {
+                        setVibrationHistory(data.patientVibration)
+                    }
+                    else
                         alert(data.message)
-                    })
-                }
-            }, [BeURL])
-  return (
-    <div>Vibration</div>
-  )
+                })
+        }
+    }, [BeURL, currentUser])
+
+
+    return (
+        <div>
+            {/* Scrollable Table */}
+            <div className="overflow-x-auto bg-white rounded-xl shadow-md">
+                <div className="">
+                    <table className="min-w-full border-collapse">
+                        <thead className="bg-teal-500 text-white sticky top-0 z-10">
+                            <tr>
+                                <th className="p-3 whitespace-nowrap border">Session Id</th>
+                                <th className="p-3 whitespace-nowrap border">Date</th>
+                                <th className="p-3 whitespace-nowrap border">Duration</th>
+                                <th className="p-3 whitespace-nowrap border">Average Temperature</th>
+
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {vibrationHistory?.length > 0 ? vibrationHistory.map((vib, idx) => (
+                                <tr
+                                    key={vib.sessionId}
+                                    className="hover:bg-teal-50 transition border-b border-slate-100 font-semibold"
+                                >
+                                    <td className="p-3 text-center whitespace-nowrap border">{vib.sessionId}</td>
+
+                                    <td className="p-3 whitespace-nowrap border text-center capitalize">{vib.date || '-'}</td>
+                                    <td className="p-3 whitespace-nowrap border text-center">{vib?.duration ? `${vib?.duration}m` : '-'}</td>
+                                    <td className="p-3 whitespace-nowrap border text-center">{`${vib.averageTemp}°C` || '-'}</td>
+
+                                </tr>
+                            )) : <tr><td colSpan="9" className="text-center p-4">No Patient Vibration History found</td></tr>}
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    )
 }
