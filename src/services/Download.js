1b370b5bf237f8fadb8254b3fb7a5144b2fa1755
Aravind2556 <aravintharavinth193@gmail.com>
2026-01-07 17:21:43 +0530

    fixing download

--- src/services/Download.js
+++ src/services/Download.js
@@ -2,7 +2,6 @@ export const vibrationDownload = async ({ BeURL, id, isDownload ,type}) => {
 
 
     if (type === "vibration" && isDownload) {
-
         try {
             const res = await fetch(`${BeURL}/fetchVibration/${id}`, {
                 method: 'GET',
@@ -67,37 +66,32 @@ export const vibrationDownload = async ({ BeURL, id, isDownload ,type}) => {
     }
     else if (type === "compress") {
         try {
-            const res = await fetch(`${BeURL}/fetchcompressor/${id}?type=${type}`,{
-                method: "GET",
-                credentials: "include",
+            const res = await fetch(`${BeURL}/fetchcompressor/${id}?type=${type}`, {
+                method: 'GET',
+                credentials: 'include'
             });
-            const contentType = res.headers.get("content-type") || "";
-            //If JSON response → no data / error → STOP download
-            if (contentType.includes("application/json")) {
-                const data = await res.json();
-                alert(data.message);
-                return;
-            }
-            
+
             if (!res.ok) {
-                throw new Error("Download failed");
+                throw new Error('Failed to download Excel');
             }
 
             const blob = await res.blob();
-            const url = window.URL.createObjectURL(blob);
 
-            const a = document.createElement("a");
+            // Create download link
+            const url = window.URL.createObjectURL(blob);
+            const a = document.createElement('a');
             a.href = url;
-            a.download = "Compression_History.xlsx";
+            a.download = 'Vibration_History.xlsx';
             document.body.appendChild(a);
             a.click();
 
+            // Cleanup
             a.remove();
             window.URL.revokeObjectURL(url);
-
-        } catch (err) {
-            console.error("Excel download error:", err);
-            alert("Failed to download Excel");
+        }
+        catch (err) {
+            console.error("Error in downloading vibration excel:", err);
+            alert("Failed to download Excel file");
         }
     }
 
